12ee3ca477953016dfbb62eb773a504f377b4949
Fisjo <ignacio.po.ipo@gmail.com>
2024-11-16 13:57:09 +0100

    sotre functions created

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,6 +20,61 @@ const initStore = () => {
     console.log('Init store <3');
 }
 
+const loadStore = () => {
+    throw new Error("Not implemented");
+}
+
+
+/**
+ * 
+ * @param {String} descripcion 
+ */
+const addTodo = (descripcion) => {
+    throw new Error("Not implemented");
+}
+
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const toggleTodo = (todoId) => {
+    throw new Error("Not implemented");
+}
+
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const deleteTodo = (todoId) => {
+    throw new Error("Not implemented");
+}
+
+
+const deleteAll = (newFilter = Filters.All) => {
+    throw new Error("Not implemented");
+}
+
+
+const setFilter = () => {
+    throw new Error("Not implemented");
+}
+
+
+const getCurrentFilter = () => {
+    throw new Error("Not implemented");
+}
+
+
+//estas funciones van a servir para manipular el store
 export default {
+    addTodo,
+    deleteAll,
+    deleteTodo,
+    getCurrentFilter,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
