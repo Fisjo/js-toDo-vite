deea5f14fe7e074db6d2f0145e04990c3f3685b6
Fisjo <ignacio.po.ipo@gmail.com>
2024-11-16 18:47:40 +0100

    store functions implemented

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -25,21 +25,44 @@ const loadStore = () => {
 }
 
 
+
+const getTodo = (filter = Filters.All) => {
+    switch (filter) {
+        case Filters.All:
+            return [...state.todos]; 
+        
+        case Filters.Completed: 
+            return state.filter(state.todos == true); 
+            
+        case Filters.Pending: 
+            return state.filter(state.todos === false);
+
+        default:
+            throw new Error(`Option ${filter} is not valid`);
+    }
+}
+
 /**
  * 
- * @param {String} descripcion 
+ * @param {String} description 
  */
-const addTodo = (descripcion) => {
-    throw new Error("Not implemented");
+const addTodo = (description) => {
+    if (!description) { throw new Error("Description is required"); }
+    state.todos.push( new Todo(description))    
 }
 
 
 /**
- * 
+ * Cambia el estado de la tarea al opuesto (hecho / no hecho)
  * @param {String} todoId 
  */
 const toggleTodo = (todoId) => {
-    throw new Error("Not implemented");
+    state.todos = state.todos.map(todo => {
+        if (todo.id === todoId) {
+            todo.done === !todo.done; 
+        }
+        return todo; 
+    })
 }
 
 
@@ -48,39 +71,37 @@ const toggleTodo = (todoId) => {
  * @param {String} todoId 
  */
 const deleteTodo = (todoId) => {
-    throw new Error("Not implemented");
+    state.todos = state.todos.filter(todo => todo.id !== todoId); 
 }
 
 
-const deleteAll = (newFilter = Filters.All) => {
-    throw new Error("Not implemented");
+const deleteCompleted = (newFilter = Filters.All) => {
+    state.todos = state.todos.filter(todo => todo.done); 
+
 }
 
-
-const setFilter = () => {
-    throw new Error("Not implemented");
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = (newFilter = Filters.All) => {
+    state.filter = newFilter; 
 }
 
 
 const getCurrentFilter = () => {
-    throw new Error("Not implemented");
+    return state.filter; 
 }
 
 
 //estas funciones van a servir para manipular el store indirectamente
 export default {
     addTodo,
-    deleteAll,
+    deleteCompleted,
     deleteTodo,
     getCurrentFilter,
-    addTodo,
-    deleteAll,
-    deleteTodo,
-    getCurrentFilter,
+    getTodo,
     initStore,
-    loadStore,
-    setFilter,
-    toggleTodo,
     loadStore,
     setFilter,
     toggleTodo,
